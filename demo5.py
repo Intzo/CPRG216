295b53abee879b7c9e4b252589b2913ec663c50c
intzo <enzojeromepascual@gmail.com>
2025-09-18 16:23:41 -0600

    problem_example_for_class

---
+++ demo5.py
@@ -0,0 +1,4 @@
+#problem 
+
+month = input("the month in using 00")
+print ("christmas is", 12 - int(month), "months away")
---
+++ problem.py
@@ -0,0 +1,4 @@
+#problem how long till christmas
+
+month = input("the month in using 00")
+print ("christmas is", 12 - int(month), "months away")
