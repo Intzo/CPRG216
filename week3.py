1ced361613083d41d300b6c4c395dddf02e95dd6
intzo <enzojeromepascual@gmail.com>
2025-09-18 16:05:10 -0600

    functions

--- week3.py
+++ week3.py
@@ -5,14 +5,18 @@ variables, operators and other stuff
 """
 #review
 #assignemnt
-
 x = 8 #int
 y = 1.2 #float
 warm = True # bool
 msg = "hello" #string of characters
-
-
 print (x,type(x))
 print (y,type(y))
 print (warm,type(warm))
 print (msg,"type of msg",type(msg))
+
+#some function call : print, input, int,float,str,bool, 
+num_as_text = "43"
+
+num_as_num = int(num_as_text)
+print (num_as_text,type(num_as_text))
+print (num_as_num,type(num_as_num))
