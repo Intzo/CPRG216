25ffd06794dd93dd6cff0b62f50280ad9d90b57b
intzo <enzojeromepascual@gmail.com>
2025-09-18 16:16:38 -0600

    birth_example

--- week3.py
+++ week3.py
@@ -20,3 +20,16 @@ num_as_text = "43"
 num_as_num = int(num_as_text)
 print (num_as_text,type(num_as_text))
 print (num_as_num,type(num_as_num))
+print (str(num_as_num)) # equivalent
+
+num = 3
+num_f = float(3)
+num2 = 3.4
+num2_i = int(num2)
+num2_as_text = str(num2)
+
+# using input function .. note input function always return a string (text)\
+
+year_of_birth = input("Please Enter your year of birth")
+print ("your age is ",2025 -  int(year_of_birth))
+
